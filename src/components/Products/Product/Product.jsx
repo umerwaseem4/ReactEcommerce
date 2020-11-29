4f1733dcbe0eecfe6109b1a7576eb925145c2ede
Your Name <umerwas4@gmail.com>
2020-11-29 22:07:09 +0500

    initial setup

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,38 @@
+// Product Component (Parent Component: Products.jsx)
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+//   IconButton,
+} from "@material-ui/core";
+
+import { AddShoppingCart } from "@material-ui/icons";
+
+import useStyles from "./styles";
+
+const Product = ({ product }) => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image="" title={product.name} />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant="h5" gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography variant="h5">{product.price}</Typography>
+        </div>
+        <Typography variant="h2" color="textSecondary">
+          {product.desc}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <AddShoppingCart />
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Product;
